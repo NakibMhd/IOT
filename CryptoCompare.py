8786d25fbc38cdf2ea1a4be94a026dd39a11945c
Nakib <Nakib@debian.fr>
2018-03-25 22:40:02 +0200

    Première version (base)

--- CryptoCompare.py
+++ CryptoCompare.py
@@ -1,1 +1,11 @@
+#!/usr/bin/env python3.5
+#-*- coding: utf-8 -*-
 import cryptocompare
+
+crypto = ''
+
+print(cryptocompare.get_coin_list(format=False))
+
+while crypto != 'exit':
+    crypto = input('De quelle cryptomonnaie souhaitez-vous connaitre le prix ? ')
+    print(cryptocompare.get_price(crypto))
